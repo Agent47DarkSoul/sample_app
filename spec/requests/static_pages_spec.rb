bcea849979770367c623897ac921ed5bcf936fe0
Danish Satkut <danish_satkut@hotmail.com>
2013-01-30 17:23:03 +0530

    Changed the routes for static pages, added contact page and added bootstrap.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,42 +3,54 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the correct title" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title', :text => 'Ruby on Rails Tutotials Sample App')
+    	visit root_path
+    	page.should have_selector('title', :text => 'Ruby on Rails Tutorials Sample App')
     end
 
     it "should not have 'Home' in the title" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should_not have_selector('title', :text => 'Home')
     end
   end
 
   describe "Help page" do
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_content('Help')
   	end
 
   	it "should have the correct title" do
-    	visit '/static_pages/help'
+    	visit help_path
     	page.should have_selector('title', :text => ' | Help')
     end
   end
 
   describe "About page" do
   	it "should have the content 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_content('About')
   	end
 
   	it "should have the correct title" do
-    	visit '/static_pages/about'
+    	visit about_path
     	page.should have_selector('title', :text => ' | About')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit contact_path
+      page.should have_content('Contact Us')
+    end
+
+    it "should have the correct title" do
+      visit contact_path
+      page.should have_selector('title', :text => ' | Contact Us')
     end
   end
 end
