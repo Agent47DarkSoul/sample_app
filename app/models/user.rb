5a8f676f72cdb96f27a3592cc190f0eb4f19e905
Danish Satkut <danish_satkut@hotmail.com>
2013-03-17 00:55:29 +0530

    Added the unique constraint on the email column for users database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  before_save { |user| user.email = email.downcase }
 end
 # == Schema Information
 #
