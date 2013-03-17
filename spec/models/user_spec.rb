c1ed7f730d4fa6bd965af10dca0579db97731cb3
Danish Satkut <danish_satkut@hotmail.com>
2013-03-17 19:43:49 +0530

    Completed authentication with tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -72,15 +77,55 @@ describe User do
 
   	it { should_not be_valid }
   end
+
+  describe "when password is not present" do
+  	before { @user.password = @user.password_confirmation = "" }
+  	it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
+  end
+
+  describe "when password_confirmation is nil" do
+  	before { @user.password_confirmation = nil }
+  	it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+  	before { @user.save }
+
+  	let(:found_user) { User.find_by_email(@user.email) }
+
+  	describe "with valid password" do
+  		it { should == found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do
+  		let(:user_for_invalid_password) { found_user.authenticate("fake password") }
+
+  		it { should_not == user_for_invalid_password }
+
+  		specify { user_for_invalid_password.should be_false }
+  	end
+  end
+
+  describe "with a password too short" do
+  	before { @user.password = @user.password_confirmation = "a" * 5 }
+
+  	it { should_not be_valid }
+  end
 end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)     indexed
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
