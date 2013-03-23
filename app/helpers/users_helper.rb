bd0f18677b8430957302627550bbc16190d6a122
Danish Satkut <danish_satkut@hotmail.com>
2013-03-23 19:22:37 +0530

    Implemented a basic profile page with user name and gravatar.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+	
+	# Public: Returns the gravatar for given user
+	# 
+	# user - The User model instance for which gravatar is required
+	# 
+	# Examples
+	# 	gravatar_for(user)
+	# 	# => <img url="https://secure.gravatar.com/avatar/6a0eec8b11d194b121ea5b84c5fd88c5" alt="Danish Satkut" class="gravatar">
+	# 	
+	# Returns the img tag with the gravatar image for the user
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Danish Satkut"
+		email "danish_satkut@hotmail.com"
+		password "foobarawesome"
+		password_confirmation "foobarawesome"
+	end
+end
