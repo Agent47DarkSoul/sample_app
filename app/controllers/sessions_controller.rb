7975be9fb5faac50b42f8a4b5743038fd3994d27
Danish Satkut <danish_satkut@hotmail.com>
2013-04-05 21:33:07 +0530

    Implemented basic signin functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.where(email: params[:session][:email]).first
+
+		if user && user.authenticate(params[:session][:password])
+			# Authenticated
+			# redirect to home
+			redirect_to user_path(user)
+		else
+			flash[:error] = "You have entered invalid username/password"
+			redirect_to signin_path
+		end
+	end
+
+	def destroy
+		
+	end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,4 +1,42 @@
 require 'spec_helper'
 
 describe "AuthenticationPages" do
+	subject { page }
+
+	describe "GET /signin page" do
+		before { get signin_path }
+
+		it "should visit signin page" do
+			response.status.should be(200)
+		end
+	end
+
+	describe "signin page" do
+		before { visit signin_path }
+
+		let(:submit) { "Sign in" }
+
+		it { should have_selector('h1', text: 'Sign in') }
+		it { should have_selector('title', text: 'Sign in') }
+
+		describe "with invalid information" do
+			before { click_button submit }
+
+			it { should have_selector('title', text: 'Sign in') }
+			it { should have_selector('div.alert.alert-error', text: 'invalid') }
+		end
+
+		describe "with valid information" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			before do
+				fill_in "Email", with: user.email
+				fill_in "Password", with: user.password
+				click_button submit
+			end
+
+			it { should have_selector('title', text: user.name) }
+			it { should_not have_link("Sign in", href: signin_path) }
+		end
+	end
 end
