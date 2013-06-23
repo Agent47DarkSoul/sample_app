87efff3e6fa5a59b051481d31a82756b7615990c
Danish Satkut <danish_satkut@hotmail.com>
2013-06-23 18:34:07 +0530

    Implemented the valid and invalid edit action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,16 @@ class UsersController < ApplicationController
 	def edit
 		@user = User.find(params[:id])
 	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile has been updated"
+			sign_in(@user)
+			redirect_to user_path(@user)
+		else
+			render :edit
+		end
+	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -74,13 +74,49 @@ describe "UserPages" do
 
   describe "profile update" do
     let(:user) { FactoryGirl.create(:user) }
+    let(:submit) { "Save changes" }
+    let(:update_h1_text) { 'Update your profile' }
 
     before { visit edit_user_path(user) }
 
     describe "page" do
-      it { should have_selector('h1', text: 'Update your profile') }
-      it { should have_selector('title', text: 'Edit user') }
-      it { should have_link('change', href: 'http://en.gravatar.com/emails') }
+      it { should have_selector('h1', :text => update_h1_text) }
+      it { should have_selector('title', :text => 'Edit user') }
+      it { should have_link('change', :href => 'http://en.gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      it "should not update the user" do
+        expect { click_button submit }.not_to change(user, :updated_at)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('h1', :text => update_h1_text) }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@email.com" }
+
+      before(:each) do
+        fill_in "Name", :with => new_name
+        fill_in "Email", :with => new_email
+        fill_in "Password", :with => user.password
+        fill_in "Confirmation", :with => user.password
+
+        click_button submit
+      end
+
+      it { should have_selector('title', :text => new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', :href => signout_path) }
+
+      specify { user.reload.name = new_name }
+      specify { user.reload.email = new_email }
     end
   end
 end
