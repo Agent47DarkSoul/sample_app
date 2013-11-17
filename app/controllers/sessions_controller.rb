b4d7661c2c875856bf7fbe73c7b2ef949e82c24e
Danish Satkut <danish_satkut@hotmail.com>
2013-11-17 13:18:34 +0530

    Added authentication api spec for signin and signout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,23 @@
 class SessionsController < ApplicationController
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		user = User.where(email: params[:session][:email]).first
+  def create
+    @user = User.where(email: params[:session][:email]).first
 
-		if user && user.authenticate(params[:session][:password])
-			# sign in the user and redirect to the user's profile page
-			sign_in user
-			redirect_to user_path(user)
-		else
-			flash[:error] = "You have entered invalid username/password"
-			redirect_to signin_path
-		end
-	end
+    if @user && @user.authenticate(params[:session][:password])
+      # sign in the user and redirect to the user's profile page
+      sign_in @user
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "You have entered invalid username/password"
+      redirect_to signin_path
+    end
+  end
 
-	def destroy
-		sign_out
-		redirect_to root_path
-	end
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
 end
--- spec/requests/authentication_api_spec.rb
+++ spec/requests/authentication_api_spec.rb
@@ -1,11 +1,57 @@
 require 'spec_helper'
 
 describe "Authentication API" do
-  describe "GET /signin page" do
-    before { get signin_path }
+  describe "sign in" do
+    shared_examples "sign in failure" do
+      it "redirects to signin page with error message" do
+        post sessions_path, :session => {
+          :email => user.email, :password => "123456"
+        }
 
-    it "should visit signin page" do
-      response.status.should be(200)
+        expect(response).to redirect_to(signin_path)
+        expect(flash[:error]).to match(/invalid username\/password/)
+      end
+    end
+
+    context "when user is available in database" do
+      let(:user) { create(:user) }
+
+      context "when user name and password is correct" do
+        it "signs in user and redirects to user's profile page" do
+          post sessions_path, :session => {
+            :email => user.email, :password => user.password
+          }
+
+          expect(response).to redirect_to(assigns(:user))
+          expect(cookies["remember_token"]).to eq(user.remember_token)
+
+          follow_redirect!
+
+          expect(response).to render_template(:show)
+        end
+      end
+
+      context "when user name or password is incorrect" do
+        include_examples "sign in failure"
+      end
+    end
+
+    context "when user is not available in database" do
+      let(:user) { build_stubbed(:user) }
+
+      include_examples "sign in failure"
+    end
+  end
+
+  describe "sign out" do
+    it "signs out user and redirects to root page" do
+      user = create(:user)
+      sign_in(user)
+
+      delete signout_path
+
+      expect(cookies["remember_token"]).not_to be_present
+      expect(response).to redirect_to(root_path)
     end
   end
 end
