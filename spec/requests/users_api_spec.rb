a72a88b7ea7cde45908df25ee36f46ba2e9205cc
Danish Satkut <danish_satkut@hotmail.com>
2013-11-17 17:52:16 +0530

    Added signup and profile spec for users.

--- spec/requests/users_api_spec.rb
+++ spec/requests/users_api_spec.rb
@@ -1,11 +1,50 @@
 require 'spec_helper'
 
-describe "Users" do
+describe "Users API" do
   describe "signup" do
-    before { get signup_path }
+    context "with valid user details" do
+      it "creates new user, signs in user and redirects to User's page" do
+        get new_user_path
+        expect(response).to render_template(:new)
 
-    it "returns correct response" do
-      expect(response.code).to eq("200")
+        user = build(:user)
+        post users_path, :user => {
+          :name => user.name,
+          :email => user.email,
+          :password => user.password,
+          :password_confirmation => user.password_confirmation
+        }
+
+        expect(response).to redirect_to(assigns(:user))
+
+        follow_redirect!
+
+        expect(response).to render_template('users/show')
+      end
+    end
+
+    context "with invalid user details" do
+      it "renders new page with errors" do
+        user = build(:user)
+        post users_path, :user => {
+          :name => user.name,
+          :password => user.password,
+          :password_confirmation => user.password_confirmation
+        }
+
+        expect(response).to render_template('users/new')
+        expect(response.body).to include('error')
+      end
+    end
+  end
+
+  describe "profile" do
+    it "returns OK response" do
+      user = create(:user)
+
+      get user_path(user)
+
+      expect(response).to be_ok
     end
   end
 end
