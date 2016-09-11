2418a68674f947e5e3055a5a7cb5a1e208a4a4b7
Danish Aziz Satkut <danish_satkut@hotmail.com>
2016-09-12 01:26:08 +0530

    Change the order of scopes for oauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 SampleApp::Application.config.middleware.use OmniAuth::Builder do
-  provider :flowdock, ENV['FLOWDOCK_CLIENT_ID'], ENV['FLOWDOCK_CLIENT_SECRET'], scope: 'profile flow integration'
+  provider :flowdock, ENV['FLOWDOCK_CLIENT_ID'], ENV['FLOWDOCK_CLIENT_SECRET'], scope: 'flow profile integration'
 end
