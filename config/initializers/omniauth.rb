3abc1f141f37233d2c25960ed606326f64cd02ea
Danish Aziz Satkut <danish_satkut@hotmail.com>
2016-09-12 00:35:53 +0530

    Change source of omniauth-oauth2 gem.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 SampleApp::Application.config.middleware.use OmniAuth::Builder do
   provider :flowdock, ENV['FLOWDOCK_CLIENT_ID'], ENV['FLOWDOCK_CLIENT_SECRET'], scope: 'profile flow integration'
 end
